1ef8a169ad06d77a0ca07ae2c0009ddc25496703
Bente Schopman <b.l.schopman@gmail.com>
2022-06-22 18:03:20 +0200

    Detail: name, kitchen, time,number of persons

--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Recipe
 
 # Create your views here.
 class Recipes(ListView):
     model = Recipe
+
+class RecipeView(DetailView):
+    model = Recipe
+    context_object_name = "detail"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Recipe_name"] = Recipe.name
+        return context
