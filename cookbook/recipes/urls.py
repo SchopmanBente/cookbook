2f48d3567cbd9e3d8cd75c64985f2a5e06aae546
Bente Schopman <b.l.schopman@gmail.com>
2022-06-22 17:35:37 +0200

    Revert "Merging"
    This reverts commit 2e9526517f4908b583512d25ef6a89d294e0c76b, reversing
    changes made to cf72b23f4652258749b4f6b0313a1aebd79d9129.

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import RecipesView, RecipeView
+from .views import Recipes
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', RecipesView.as_view(), name="index"),
-    path("recipes/<int:pk>",RecipeView.as_view(), name="detail")
+    path('', Recipes.as_view(), name="index"),
 ]
--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -1,19 +1,9 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.utils import timezone
-from django.views.generic.detail import DetailView
+
 from .models import Recipe
 
 # Create your views here.
-class RecipesView(ListView):
+class Recipes(ListView):
     model = Recipe
-
-class RecipeView(DetailView):
-    model = Recipe
-    context_object_name = "detail"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["name"] = Recipe.name
-        return context
