eaeb9c78964c947ffa23b29981075ff2c285ae13
Bente Schopman <b.l.schopman@gmail.com>
2022-06-22 16:11:24 +0200

    Added links in index

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import Recipes
+from .views import RecipesView, RecipeView
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', Recipes.as_view(), name="index"),
+    path('', RecipesView.as_view(), name="index"),
+    path("recipes/<int:pk>",RecipeView.as_view(), name="detail")
 ]
--- cookbook/recipes/views.py
+++ cookbook/recipes/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views.generic import ListView
-
+from django.utils import timezone
+from django.views.generic.detail import DetailView
 from .models import Recipe
 
 # Create your views here.
-class Recipes(ListView):
+class RecipesView(ListView):
     model = Recipe
+
+class RecipeView(DetailView):
+    model = Recipe
+    context_object_name = "detail"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Recipe"] = Recipe.object
+        return context
