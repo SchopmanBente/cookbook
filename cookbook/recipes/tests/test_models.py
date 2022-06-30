4a7f55af832172648d827444f7b423c4e6b58db4
Bente Schopman <b.l.schopman@gmail.com>
2022-06-30 17:57:56 +0200

    adding test to check if the strings are the same

--- cookbook/recipes/tests/test_models.py
+++ cookbook/recipes/tests/test_models.py
@@ -14,7 +14,13 @@ class QuantityModelTestCase(TestCase):
         self.assertIsInstance(self.quantity, Quantity)
         self.assertIsInstance(isinstance(self.quantity.quantity, int), int)
 
+    def test_quantity_string_is_string_of_quantity(self):
+        self.assertIs(type(self.quantity.__str__()), str)
 
+    """ 
+    def test_quantity_string_is_the_same(self):
+        self.assertIs(self.quantity.__str__(), str(self.quantity.quantity))
+    """
 class UnitModelTestCase(TestCase):
     def setUp(self):
         self.unit = baker.make('recipes.Unit')
@@ -22,6 +28,10 @@ class UnitModelTestCase(TestCase):
     def test_unit_consists_of_Unit_and_string(self):
         self.assertIsInstance(self.unit, Unit)
         self.assertTrue(isinstance(self.unit.unit, str), str)
+
+    def test_unit_prints_string(self):
+        self.assertIsInstance(self.unit.unit, str)
+        self.assertTrue(id(self.unit.__str__()),id(self.unit.unit))
 
 
 class TimeModelTestCase(TestCase):
@@ -33,6 +43,8 @@ class TimeModelTestCase(TestCase):
         self.assertIsInstance(self.time.hours, int)
         self.assertIsInstance(self.time.minutes, int)
 
+    def test_time_prints_string(self):
+        self.assertTrue(id(self.time.__str__()), id("Uren:{hours} Minuten: {minutes}".format(hours=str(self.time.hours), minutes=str(self.time.minutes))))
 
 class NecessityModelTestCase(TestCase):
     def setUp(self):
@@ -41,6 +53,9 @@ class NecessityModelTestCase(TestCase):
     def test_necessity_consists_of_Necessity_and_string(self):
         self.assertIsInstance(self.necessity, Necessity)
         self.assertIsInstance(self.necessity.necessity_name, str)
+
+    def test_necessity_prints_string(self):
+        self.assertTrue(self.necessity.__str__(), self.necessity.necessity_name)
 
 
 class IngredientModelTestCase(TestCase):
@@ -51,6 +66,8 @@ class IngredientModelTestCase(TestCase):
         self.assertIsInstance(self.ingredient, Ingredient)
         self.assertIsInstance(self.ingredient.ingredient_name, str)
 
+    def test_ingredient_prints_string(self):
+        self.assertIs(self.ingredient.__str__(), self.ingredient.ingredient_name)
 
 class QuantityUnitIngredientModelTestCase(TestCase):
     def setUp(self):
@@ -58,6 +75,9 @@ class QuantityUnitIngredientModelTestCase(TestCase):
 
     def test_hunderd_gram_gehakt_consists_of_QuantityUnitIngredient(self):
         self.assertIsInstance(self.hunderd_gram_gehakt, QuantityUnitIngredient)
+
+    def test_QuantityUnitIngredient_prints_string(self):
+        self.assertTrue(id(self.hunderd_gram_gehakt.__str__()), id("{quantity}  {unit} {ingredient}".format(ingredient=self.hunderd_gram_gehakt.ingredient,quantity=self.hunderd_gram_gehakt.quantity,unit=self.hunderd_gram_gehakt.unit)))
 
 
 class QuantityUnitNecessityModelTestCase(TestCase):
@@ -67,7 +87,9 @@ class QuantityUnitNecessityModelTestCase(TestCase):
     def test_one_stuk_snijplank_consists_of_QuantityUnitNecessity(self):
         self.assertIsInstance(self.one_stuk_snijplank, QuantityUnitNecessity)
 
-
+    def test_QuantityUnitNecessity_prints_the_same_string(self):
+        self.assertTrue(id(self.one_stuk_snijplank.__str__()), id("{quantity}  {unit}  {necessity}".format(necessity=self.one_stuk_snijplank.necessity, quantity=self.one_stuk_snijplank.quantity,
+                                                        unit=self.one_stuk_snijplank.unit)))
 class KitchenModelTestCase(TestCase):
     def setUp(self):
         self.kitchen = baker.make('recipes.Kitchen')
@@ -75,6 +97,9 @@ class KitchenModelTestCase(TestCase):
         self.assertIsInstance(self.kitchen, Kitchen)
         self.assertIsInstance(self.kitchen.name, str)
         self.assertIsInstance(self.kitchen.description, str)
+
+    def test_kitchen_prints_the_same_string(self):
+        self.assertTrue(id(self.kitchen.__str__()), id(self.kitchen.name))
 
 
 class RecipeModelTestCase(TestCase):
@@ -107,27 +132,6 @@ class RecipeModelTestCase(TestCase):
         self.assertEqual(self.recipe.necessities.count(),1)
 
 
-class AddQuantityUnitIngredient(TestCase):
-    def setUp(self):
-        self.quantity = Quantity.objects.create(quantity=100)
-        self.unit = Unit.objects.create(unit="gram")
-        self.ingredient = Ingredient.objects.create(ingredient_name="komkommer")
-        self.hunderd_gram_komkommer = QuantityUnitIngredient(ingredient=self.ingredient,quantity=self.quantity, unit=self.unit )
+    def test_recipe_prints_the_same_string(self):
+        self.assertTrue(id(self.recipe.__str__()), id(self.recipe.name))
 
-    def test_check_types(self):
-        self.assertIsInstance(self.quantity,Quantity)
-        self.assertIsInstance(self.unit,Unit)
-        self.assertIsInstance(self.ingredient,Ingredient)
-        self.assertIsInstance(self.hunderd_gram_komkommer,QuantityUnitIngredient)
-
-class  AddQuantityUnitIngredientToRecipe(TestCase):
-    def setUp(self):
-        self.recipe = baker.make('recipes.Recipe')
-        self.recipe.save()
-        self.ingredient1 = baker.make('recipes.QuantityUnitIngredient', make_m2m=True)
-        self.ingredient1.save()
-        pprint(self.ingredient1.__dict__)
-        self.recipe.ingredients.add(self.ingredient1)
-
-    def test_recipe_has_ingredients(self):
-        self.assertEqual(self.recipe.ingredients.count(), 1)
