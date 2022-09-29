bd093afbe841e240c16e00b422d19a0776e40398
Bente Schopman <b.l.schopman@gmail.com>
2022-09-29 12:27:40 +0200

    All fields of the Recipe model in the filter

--- cookbook/recipes/filters.py
+++ cookbook/recipes/filters.py
@@ -1,16 +1,19 @@
 import django_filters
-from django_filters import ModelMultipleChoiceFilter
+from django_filters import MultipleChoiceFilter
 
-from .models import Recipe,Kitchen
+from .models import Recipe
+
 
 class RecipeFilter(django_filters.FilterSet):
     class Meta:
         model = Recipe
         fields = {
             "name": ["icontains"],
-            "kitchen": [ "exact"] ,
-            "number_of_persons": ["exact"] ,
-
-
+            "time": ["exact"],
+            "kitchen": ["exact"],
+            "number_of_persons": ["exact"],
+            "ingredients": ["exact"],
+            "necessities": ["exact"],
+            "preparation": ["icontains"],
         }
 
